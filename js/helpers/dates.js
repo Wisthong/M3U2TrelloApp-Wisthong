3ca6bab95268b89207edf1f7c5f39461ba16104e
Wisthong Martinez Castro <wisthongd@gmail.com>
2022-07-31 17:23:48 -0500

    Probando timestamp

--- js/helpers/dates.js
+++ js/helpers/dates.js
@@ -1,4 +1,3 @@
 const dateFormat = (time, format = 'DD/MM/YYYY') => {
-    // Con el moment.unix generamos una fecha a partir de un timestamp y luego aplicamos el format 
     return moment.unix(time).format(format);
   };
